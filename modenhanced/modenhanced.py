67ccf5756b634c6bf1d92c89b2f3bd5954cff7e7
Nop0x <st4lk3r1992@gmail.com>
2016-12-02 17:02:59 +0100

    Update modenhanced.py

--- modenhanced/modenhanced.py
+++ modenhanced/modenhanced.py
@@ -1289,24 +1289,24 @@ class modenhanced:
         ts = datetime.datetime.now().strftime('%H:%M:%S') 
         await self.appendmodlog_ne("`" +ts + "` :no_entry: __**" + member.name +"#"+ str(member.discriminator)+"**__ *(" + member.id+")* **left the server**", member.server)
         
-    async def rate_limit(self, message):
-        rate = 5.0; // unit: messages
-        per  = 8.0; // unit: seconds
-        allowance = rate; // unit: messages
-        last_check = now(); // floating-point, e.g. usec accuracy. Unit: seconds
-
-        when (message_received):
-            current = now();
-            time_passed = current - last_check;
-            last_check = current;
-            allowance += time_passed * (rate / per);
-            if (allowance > rate):
-                allowance = rate; // throttle
-            if (allowance < 1.0):
-                discard_message();
-            else:
-                forward_message();
-                allowance -= 1.0;
+    #async def rate_limit(self, message):
+     #   rate = 5.0; // unit: messages
+      #  per  = 8.0; // unit: seconds
+       # allowance = rate; // unit: messages
+        #last_check = now(); // floating-point, e.g. usec accuracy. Unit: seconds
+#
+ #       when (message_received):
+  #          current = now();
+   #         time_passed = current - last_check;
+    #        last_check = current;
+     #       allowance += time_passed * (rate / per);
+      #      if (allowance > rate):
+       #         allowance = rate; // throttle
+        #    if (allowance < 1.0):
+         #       discard_message();
+          #  else:
+           #     forward_message();
+            #    allowance -= 1.0;
     
     async def on_message_delete(self, message):
         if message.channel.is_private or self.bot.user == message.author:
