387dc39ebb35771b393182ea8391a7ca5109c5ce
Nop0x <st4lk3r1992@gmail.com>
2016-12-02 20:07:43 +0100

    Update modenhanced.py

--- modenhanced/modenhanced.py
+++ modenhanced/modenhanced.py
@@ -258,7 +258,7 @@ class modenhanced:
             data.set_image(url="http://gameranx.com/wp-content/uploads/2016/05/Overwatch6.jpg")
             data.add_field(name="Action: Banned " + user.name + " from the server", value="Reason: " + reason)
             self._tmp_banned_cache.append(user)
-            # await self.bot.ban(user, days)
+            await self.bot.ban(user, days)
             await self.appendmodlog(data, server)
             await self.bot.say("Done. It was about time.")
         except discord.errors.Forbidden:
@@ -281,7 +281,7 @@ class modenhanced:
             data.set_author(name="Automatic Filter Action")
             data.add_field(name="Action: Banned " + user.name + " from the server", value="Reason: " + reason)
             self._tmp_banned_cache.append(user)
-            # await self.bot.ban(user, days)
+            await self.bot.ban(user, days)
             await self.appendmodlog(data, server)
         except Exception as e:
             print(e)
@@ -626,6 +626,18 @@ class modenhanced:
             await self.bot.say("This server has been removed from the ignore list.")
         else:
             await self.bot.say("This server is not in the ignore list.")
+
+    @commands.command(name="mute", pass_context=True)
+    async def botswap(self,ctx, member:discord.Member, channel:discord.Channel):
+        try:
+            await self.bot.ban(member, 0)
+            data = discord.Embed(colour=discord.Colour.red())
+            data.set_author(name="Moderation Log")
+            data.set_image(url="http://gameranx.com/wp-content/uploads/2016/05/Overwatch6.jpg")
+            data.add_field(name="Action: Banned D.Va from the Server. There is only room for one Modbot, bitch.")
+            await self.bot.say(channel, embed=data)
+        except:
+            return
 
     @commands.command(name="mute", pass_context=True)
     async def manual_mute(self, ctx, member: discord.Member, duration: int, unit: str, reason: str):
